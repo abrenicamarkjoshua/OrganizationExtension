fffbfe24d2d6c88b87e58b403abbbc09cb1e373a
joshua <abrenicamarkjoshua@gmail.com>
2016-02-19 16:20:47 +0800

    saving

---
+++ PMTaskTableExtension.cs
@@ -0,0 +1,25 @@
+﻿using PX.Data;
+using PX.Objects.PM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeStructure
+{
+    [PXTable(typeof(PMTask.taskID),
+        IsOptional = true)]
+    public class PMTaskTableExtension : PXCacheExtension<PMTask>
+    {
+        #region ParentID
+        public abstract class parentID : PX.Data.IBqlField
+        {
+        }
+        [PXDBInt]
+        [PXDefault(0)]
+        [PXUIField(DisplayName = "Parent Task ID")]
+        public string ParentID { get; set; }
+        #endparentid
+    }
+}
